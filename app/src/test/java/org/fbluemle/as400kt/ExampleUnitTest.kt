adbbc82adcc1fb1ce844f4dd6ef4c407d4277035
Frieder Bluemle <frieder.bluemle@gmail.com>
2020-05-28 18:44:19 -0700

    Fix whitespace errors and formatting

--- app/src/test/java/org/fbluemle/as400kt/ExampleUnitTest.kt
+++ app/src/test/java/org/fbluemle/as400kt/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package org.fbluemle.as400kt
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
